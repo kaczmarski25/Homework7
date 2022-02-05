4e4f701cc14d31f6e06e147903d87f2f1b6999e1
kaczmarski <eukaczmarski@gmail.com>
2022-02-05 17:42:15 +0300

    Start HomeWork7

---
+++ src/geekbrains_home_work/Main.java
@@ -0,0 +1,12 @@
+package geekbrains_home_work;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cat cat = new Cat("Barsik", 5);
+        Plate plate = new Plate(100);
+        plate.info();
+        cat.eat(plate);
+        plate.info();
+    }
+}
---
+++ src/geekbrains_home_work/Plate.java
@@ -0,0 +1,14 @@
+package geekbrains_home_work;
+
+public class Plate {
+    private int food;
+    public Plate(int food) {
+        this.food = food;
+    }
+    public void decreaseFood(int n) {
+        food -= n;
+    }
+    public void info() {
+        System.out.println("plate: " + food);
+    }
+}
